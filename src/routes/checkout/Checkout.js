8f8f48f17ec6323420ceb991549bb7c0f28fec05
Felix <nampanya.f@northeastern.edu>
2022-04-22 22:02:52 -0400

    added checkout

--- src/routes/checkout/Checkout.js
+++ src/routes/checkout/Checkout.js
@@ -1,25 +1,36 @@
 import './checkout.styles.scss';
 import { CartContext } from '../../contexts/CartProvider';
 import { useContext } from 'react';
+import CheckoutItem from '../../components/checkout-item/CheckoutItem';
 
 const Checkout = () => {
 
-    const { cartItems, addItemToCart, removeItemFromCart } = useContext(CartContext);
+    const { cartItems, cartTotal } = useContext(CartContext);
     return (
-        <div>
-            <h1> check out</h1>
-            <div>
-                {cartItems.map((cartItem) => {
-                    const { id, name, quantity } = cartItem;
-                    return (<div key={id}>
-                        <h2>{name} </h2>
-                        <span>{quantity}</span>
-                        <span onClick={()=> removeItemFromCart(cartItem) }>decrement </span>
-                        <br/>
-                        <span onClick={()=> addItemToCart(cartItem) }>increment </span>
-                    </div>)
-                })}
+        <div className='checkout-container'>
+            <div className='checkout-header'>
+                <div className='header-block'>
+                    <span>Product</span>
+                </div>
+                <div className='header-block'>
+                    <span>Description</span>
+                </div>
+                <div className='header-block'>
+                    <span>Quantity</span>
+                </div>
+                <div className='header-block'>
+                    <span>Price</span>
+                </div>
+                <div className='header-block'>
+                    <span>Remove</span>
+                </div>
             </div>
+
+            {cartItems.map((cartItem) => (
+                <CheckoutItem key={cartItem.id} cartItem={cartItem} />
+            ))}
+            <span className='total'>Total: ${cartTotal}</span>
+
         </div>
     );
 };
